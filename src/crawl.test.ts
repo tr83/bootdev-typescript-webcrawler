694e5845789e0b737de01fa8ef31ad327c62e031
tr83 <terorii@gmail.com>
2026-03-11 21:53:02 +0200

    heading and paragraph scraping

--- src/crawl.test.ts
+++ src/crawl.test.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "vitest";
-import { normalizeURL } from "./crawl";
+import { normalizeURL, getHeadingFromHTML, getFirstParagraphFromHTML } from "./crawl";
 
 test("normalizeURL protocol", () => {
     const input = "https://crawler-test.com/path";
@@ -26,5 +26,54 @@ test("normalizeURL http", () => {
     const input = "http://CRAWLER-TEST.com/path";
     const actual = normalizeURL(input);
     const expected = "crawler-test.com/path";
+    expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML returns H1 tag content", () => {
+    const input = "<html><head><title>Test</title></head><body><h1>Heading</h1></body></html>";
+    const actual = getHeadingFromHTML(input);
+    const expected = "Heading";
+    expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML returns H1 tag content, ignoring H2", () => {
+    const input = "<html><head><title>Test</title></head><body><h1>Heading</h1><h2>Subheading</h2></body></html>";
+    const actual = getHeadingFromHTML(input);
+    const expected = "Heading";
+    expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML returns H2 tag content as fallback if H1 is not present", () => {
+    const input = "<html><head><title>Test</title></head><body><h2>Heading H2</h2></body></html>";
+    const actual = getHeadingFromHTML(input);
+    const expected = "Heading H2";
+    expect(actual).toEqual(expected);
+});
+
+test("getHeadingFromHTML returns empty string if neither an <h1> nor an <h2> tag is found", () => {
+    const input = "<html><head><title>Test</title></head><body><p>Content</p></body></html>";
+    const actual = getHeadingFromHTML(input);
+    const expected = "";
+    expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML returns the main-element's first paragraph", () => {
+    const input = "<html><head><title>Test</title></head><body><main><p>First paragraph</p><p>Second paragraph</p></main></body></html>";
+    const actual = getFirstParagraphFromHTML(input);
+    const expected = "First paragraph";
+    expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML returns the first paragraph if the main element is not found", () => {
+    const input = "<html><head><title>Test</title></head><body><p>First paragraph</p><p>Second paragraph</p></body></html>";
+    const actual = getFirstParagraphFromHTML(input);
+    const expected = "First paragraph";
+    expect(actual).toEqual(expected);
+});
+
+test("getFirstParagraphFromHTML returns the first paragraph even if it's further down the page", () => {
+    const input = "<html><head><title>Test</title></head><body><div><a>Anchor</a></div><div><p>First paragraph</p><p>Second paragraph</p></div></body></html>";
+    const actual = getFirstParagraphFromHTML(input);
+    const expected = "First paragraph";
     expect(actual).toEqual(expected);
 });
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,3 +1,5 @@
+import { JSDOM } from 'jsdom'
+
 export function normalizeURL(url: string) {
     const urlObj = new URL(url);
     let fullPath = `${urlObj.host}${urlObj.pathname}`;
@@ -7,4 +9,36 @@ export function normalizeURL(url: string) {
     }
 
     return fullPath;
+}
+
+export function getHeadingFromHTML(html: string) {
+    const dom = new JSDOM(html);
+    const h1 = dom.window.document.querySelector("h1");
+
+    if (h1) {
+        return h1.textContent;
+    }
+
+    const h2 = dom.window.document.querySelector("h2");
+
+    if (h2) {
+        return h2.textContent;
+    }
+
+    return '';
+}
+
+export function getFirstParagraphFromHTML(html: string) {
+    const dom = new JSDOM(html);
+    const main = dom.window.document.querySelector("main");
+
+    if (main) {
+        const p = main.querySelector("p");
+        if (p) {
+            return p.textContent;
+        }
+    }
+
+    const p = dom.window.document.querySelector("p");
+    return p ? p.textContent : '';
 }
