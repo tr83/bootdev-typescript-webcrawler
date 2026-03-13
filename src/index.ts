6ec872d88ccbdc33c55d286741f30cb624b0b75d
tr83 <terorii@gmail.com>
2026-03-13 22:05:13 +0200

    Main function with CLI arguments checks

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,19 @@
-import { normalizeURL } from "./crawl";
+import { argv } from 'node:process';
 
-console.log("Hello, World!");
+function main() {
+    if (argv.length < 3) {
+        console.error("Please provide a URL as an argument.");
+        process.exit(1);
+    }
 
-normalizeURL("https://www.example.com/path/");
+    if (argv.length > 3) {
+        console.error("Too many arguments provided. Please provide only one URL.");
+        process.exit(1);
+    }
+
+    const baseUrl = argv[2];
+    console.log(`Starting crawl of: ${baseUrl}`);
+    process.exit(0);
+}
+
+main();
